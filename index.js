50c301561e8b1a22190ffefdfb5ddc5c835d7649
suyogschavan <suyogschavan03@gmail.com>
2023-12-16 16:17:22 +0530

    Final Changes

--- index.js
+++ index.js
@@ -8,10 +8,18 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
 app.get('/', async (req, res) => {
     try {
-        const response = await axios.get(`https://api.adzuna.com/v1/api/jobs/in/search/1?app_id=de5667c5&app_key=8cef77a5982edfe07967b4ef3e7c1bed&results_per_page=100&sort_by=salary`);
+        const response = await axios.get(`https://api.adzuna.com/v1/api/jobs/in/search/1?app_id=de5667c5&app_key=8cef77a5982edfe07967b4ef3e7c1bed&results_per_page=50&sort_by=salary`);
+        res.render('index', { jobs: response.data.results });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+app.get('/home', async (req, res) => {
+    try {
+        const response = await axios.get(`https://api.adzuna.com/v1/api/jobs/in/search/1?app_id=de5667c5&app_key=8cef77a5982edfe07967b4ef3e7c1bed&results_per_page=50&sort_by=salary`);
         res.render('index', { jobs: response.data.results });
     } catch (error) {
         console.error(error);
@@ -26,10 +34,13 @@ app.get('/search', async (req, res) => {
             return res.render('search', { error: 'Please enter both job title and location.' });
         }
 
-        const response = await axios.get(`https://api.adzuna.com/v1/api/jobs/in/search/1?app_id=de5667c5&app_key=8cef77a5982edfe07967b4ef3e7c1bed&results_per_page=10&sort_by=salary&what=${jobTitle}&where=${location}`);
+        const response = await axios.get(`https://api.adzuna.com/v1/api/jobs/in/search/1?app_id=de5667c5&app_key=8cef77a5982edfe07967b4ef3e7c1bed&results_per_page=100&sort_by=salary&what=${jobTitle}&where=${location}`);
         const jobs = response.data.results;
 
-        res.render('search', { jobs });
+        res.render('search', { 
+            jobs: jobs,
+            data2: {jobTitle, location}
+         });
     } catch (error) {
         console.error(error);
 
---
+++ views/header.js
@@ -0,0 +1,46 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <style>
+          nav {
+            height: 40px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color:  #0a0a23;
+          }
+  
+          ul {
+            padding: 0;
+          }
+          
+          a {
+            font-weight: 700;
+            margin: 0 25px;
+            color: #fff;
+            text-decoration: none;
+          }
+          
+          a:hover {
+            padding-bottom: 5px;
+            box-shadow: inset 0 -2px 0 0 #fff;
+          }
+        </style>
+        <header>
+          <nav>
+            <ul>
+              <li><a href="about.html">About</a></li>
+              <li><a href="work.html">Work</a></li>
+              <li><a href="contact.html">Contact</a></li>
+            </ul>
+          </nav>
+        </header>
+      `;
+    }
+  }
+  
+  customElements.define('header-component', Header);
